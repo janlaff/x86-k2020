30971773d365a391439d605ffdcf90973db41c5d
Jan Lafferton <jan@lafferton.de>
2020-10-18 14:07:24 +0200

    Assembler kernel entry point

---
+++ boot.asm
@@ -0,0 +1,38 @@
+; Various constants needed for the multiboot header
+MBALIGN  equ 1 << 0
+MEMINFO  equ 1 << 1
+FLAGS    equ MBALIGN | MEMINFO
+MAGIC    equ 0x1BADB002
+CHECKSUM equ -(MAGIC + FLAGS)
+
+; The first bytes of the kernel are the ones of the multiboot section
+; They allow the bootloader to identify the boot options of the kernel
+section .multiboot
+align 4
+    dd MAGIC
+    dd FLAGS
+    dd CHECKSUM
+
+; The kernel stack (16 KiB) is allocated in this section
+section .bss
+align 16
+stack_bottom:
+    resb 16386
+stack_top:
+
+section .text
+global _start:function (_start.end - _start)
+_start:
+    ; Initialize the esp register with the stack starting address
+    mov esp, stack_top
+    ; Define C starting point
+    extern kernel_main
+    ; Call into the c code
+    call kernel_main
+
+    ; Endless loop
+    cli
+.hang:
+    hlt
+    jmp .hang
+.end:
