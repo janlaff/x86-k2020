82afc50fc80b0f55606a418a138ff03f5fad6965
tim-fa <tim.farahani@gmail.com>
2020-10-25 12:34:24 +0100

    Add libc strlen

--- libc/include/string.h
+++ libc/include/string.h
@@ -6,4 +6,7 @@
 // copy the contents of src into the destination buffer
 void* memcpy(void* dest, const void* src, size_t size);
 
+// returns the length of a null terminated string
+size_t strlen(const char* string);
+
 #endif // LIBC_STRING_H
---
+++ libc/string/strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+
+size_t strlen(const char* string)
+{
+    int length = 0;
+    while(string[length])
+    {
+        length++;
+    }
+    return length;
+}
